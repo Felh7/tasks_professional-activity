44d81b0718398680f3474245244b0722a1ed356f
я <bakurovnikita7@gmail.com>
2023-04-20 17:53:13 +0300

    добавил комменты

--- эцп/проверка.py
+++ эцп/проверка.py
@@ -1,17 +1,21 @@
 import rsa
 from hashlib import sha256
 hashed = sha256()
-
+#считывание ключа
 with open("key_s.pem", "rb") as file:
     keydata = file.read()
 pubkey = rsa.PublicKey.load_pkcs1(keydata)
 
+#считывание подписи
 with open("gh.bin", "rb") as file:
     signed = file.read()
+# хеширование исходного сообщение
 with open("практика 4_8 .txt", "rb") as file:
     buf = file.read()
     hashed.update(buf)
     hashed_message= hashed.digest()
+
+#сравнение хешированного сообщения и подписи
 try:
     if rsa.verify(hashed_message, signed, pubkey):
         print("успешно")
--- эцп/эцп.py
+++ эцп/эцп.py
@@ -1,14 +1,23 @@
-from hashlib import sha256
-import rsa
-(pubkey, privkey) = rsa.newkeys(1024)
+from hashlib import sha256 #hashlib
+import rsa #rsa
+
+(pubkey, privkey) = rsa.newkeys(1024) #создание ключей
+
 hashed = sha256()
+
+#запись открытого ключа в файл
+
 with open("key_s.pem", "wb") as file:
     file.write(pubkey.save_pkcs1())
+
+#хеширование исходных данных
 
 with open ("практика 4_8 .txt", "rb") as f:
     cr = f.read()
     hashed.update(cr)
     message_hashed = hashed.digest()
     signed = rsa.sign(message_hashed, privkey, 'SHA-256')
+
+#запись подписи в файл
 with open ("gh.bin", "wb") as f:
     f.write(signed)
